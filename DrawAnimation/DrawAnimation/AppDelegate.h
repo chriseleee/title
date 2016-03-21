8397f194181ae342b348736e96bc4bf7a4b9a803
chrise <chrise@chrisedeMac-mini.local>
2016-03-21 18:37:25 +0800

    first commit

---
+++ DrawAnimation/DrawAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrawAnimation
+//
+//  Created by chrise on 16/2/16.
+//  Copyright © 2016年 chrise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DrawAnimation/DrawAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DrawAnimation
+//
+//  Created by chrise on 16/2/16.
+//  Copyright © 2016年 chrise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
